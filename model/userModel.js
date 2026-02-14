93fd25f00b77fa07baf16b97bc85dcf8697b8a5c
Yogesh khadka <kcyogesh7007@gmail.com>
2026-02-14 21:38:09 +0545

    register and login user

---
+++ model/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: [true, "Username must be provided"],
+    },
+    userEmail: {
+      type: String,
+      required: [true, "userEmail must be provided"],
+    },
+    userPassword: {
+      type: String,
+      required: [true, "userPassword must be provided"],
+    },
+    userPhoneNumber: {
+      type: String,
+      required: [true, "userPhoneNumber must be provided"],
+    },
+    role: {
+      type: String,
+      enum: ["customer", "admin"],
+      default: "admin",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
