dc8e96ddf428d3e63a5457330a4e1a08ad5f1d43
Yogesh khadka <kcyogesh7007@gmail.com>
2026-02-15 15:20:27 +0545

    Add image handling

--- controller/admin/product/productController.js
+++ controller/admin/product/productController.js
@@ -8,6 +8,14 @@ exports.createProduct = async (req, res) => {
     productDescription,
     productStockQty,
   } = req.body;
+  const file = req.file;
+  let filePath;
+  if (!file) {
+    filePath =
+      "https://imgs.search.brave.com/pjt9Yr-bQVIk6vy-04yC7OgmFzXxMG-44ONTW--7SWY/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9jZG4u/c2hvcGlmeS5jb20v/cy9maWxlcy8xLzAw/NzAvNzAzMi9hcnRp/Y2xlcy93aGF0XzIw/aXNfMjBhXzIwcHJv/ZHVjdF8xODQ4eDk3/MF8yYzllODFhYy1m/NmUwLTQ1ZTQtODY0/My02NjU1ZTM4MWI2/Y2MucG5nP3Y9MTc2/MzQxMTA3OSZvcmln/aW5hbFdpZHRoPTE4/NDgmb3JpZ2luYWxI/ZWlnaHQ9Nzgy";
+  } else {
+    filePath = req.file.filename;
+  }
   if (
     !productName ||
     !productPrice ||
@@ -26,6 +34,7 @@ exports.createProduct = async (req, res) => {
     productStatus,
     productDescription,
     productStockQty,
+    productImage: "http://localhost:3000/" + filePath,
   });
   res.status(201).json({
     message: "Product created successfully",
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,11 +3,18 @@ const {
 } = require("../controller/admin/product/productController");
 const isAuthenticated = require("../middleware/isAuthenticated");
 const isRestrictTo = require("../middleware/isRestrictTo");
+const { multer, storage } = require("../middleware/multerConfig");
+const upload = multer({ storage });
 
 const router = require("express").Router();
 
 router
   .route("/products")
-  .post(isAuthenticated, isRestrictTo("admin"), createProduct);
+  .post(
+    isAuthenticated,
+    isRestrictTo("admin"),
+    upload.single("productImage"),
+    createProduct,
+  );
 
 module.exports = router;
